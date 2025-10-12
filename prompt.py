2a4e218a4b0d26b1d5b85743c1e9a86abdb40345
Stelios Tsesmetzis <stylianos.tsesmetzis@gmail.com>
2025-10-12 21:24:48 +0300

    Update ReAct prompt for gemma3:12b

--- prompt.py
+++ prompt.py
@@ -6,9 +6,9 @@ Answer the following questions as best you can. You have access to the following
 Use the following format:
 
 Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
+Thought: you should always think about what to do, CRITICAL: Your next step MUST be an action. Do not skip the 'Action:' tag.
+Action: the action to take, should be one of [{tool_names}], CRITICAL: Your next step MUST be an action input. Do not skip the 'Action Input:' tag.
+Action Input: the input to the action, don't skip Action input even if it is empty
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
