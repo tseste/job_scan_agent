25bea7c6b57a1a42b51df221ba78e42511ec8d10
Stelios Tsesmetzis <stylianos.tsesmetzis@gmail.com>
2025-10-13 22:56:04 +0300

    Replace direct pydantic parsing with langchain built-in method

--- agent.py
+++ agent.py
@@ -1,12 +1,10 @@
 from dotenv import load_dotenv
-from langchain_core import output_parsers
-from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
@@ -16,13 +14,13 @@ from schemas import AgentResponse
 
 tools = [TavilySearch()]
 
-llm = ChatOllama(temperature=0, model="gemma3:12b")
+llm = ChatOllama(model="qwen3:14b")
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("tseste/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tools", "tool_names"]
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 agent = create_react_agent(
     llm=llm,
@@ -31,8 +29,7 @@ agent = create_react_agent(
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, return_intermediate_steps=True, handle_parsing_errors=True, verbose=True)
 extract_ouput = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_ouput | parse_output
+chain = agent_executor | extract_ouput | structured_llm
 
 
 def main():
